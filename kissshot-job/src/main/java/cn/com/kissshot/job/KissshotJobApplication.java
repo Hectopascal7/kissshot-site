551faab1ae514655dd869e8699d5e91b53ee4393
asuna7 <hectopascal@yeah.net>
2024-01-08 00:48:39 +0800

    包扫描路径修改。

--- kissshot-job/src/main/java/cn/com/kissshot/job/KissshotJobApplication.java
+++ kissshot-job/src/main/java/cn/com/kissshot/job/KissshotJobApplication.java
@@ -4,8 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("cn.com.kissshot.mapper.*")
-@SpringBootApplication(scanBasePackages = "cn.com.kissshot.*")
+@MapperScan("cn.com.kissshot.mapper")
+@SpringBootApplication(scanBasePackages = "cn.com.kissshot")
 public class KissshotJobApplication {
 
     public static void main(String[] args) {
